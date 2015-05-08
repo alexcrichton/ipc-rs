e721900eb27d636f67058298aed203d20190fbee
a.navrotskiy <navrotskiy@corp.mail.ru>
2015-05-08 19:20:54 +0300

    More correct test

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -14,8 +14,10 @@ fn main() {
             "test1_inner" => {
                 let sem1 = ipc::Semaphore::new("foo1", 0).unwrap();
                 let sem2 = ipc::Semaphore::new("foo2", 0).unwrap();
+                println!("[1] Unlock foo2");
                 let _ = sem2.release();
                 let _ = sem1.access();
+                println!("[1] Lock foo1");
             }
             "test1" => first_pass(),
             v => panic!("Unknown test: {}", v),
@@ -31,11 +33,16 @@ fn me() -> Command {
 fn first_pass() {
     let sem1 = ipc::Semaphore::new("foo1", 1).unwrap();
     let sem2 = ipc::Semaphore::new("foo2", 0).unwrap();
+    println!("[0] Lock foo1");
     let g1 = sem1.access();
+    println!("[0] Start");
     let mut p = me().arg("test1_inner").spawn().unwrap();
     sem2.acquire();
+    println!("[0] Lock foo2");
+    println!("[0] Unlock foo1");
     drop(g1);
     p.wait().unwrap();
+    println!("[0] Join");
     drop(sem1.access());
 }
 
@@ -57,8 +64,15 @@ mod tests {
         assert! (output.status.success());
         assert_eq! (str::from_utf8(&output.stdout).unwrap(), 
 r#"Enter: test1
+[0] Lock foo1
+[0] Start
 Enter: test1_inner
+[1] Unlock foo2
+[0] Lock foo2
+[0] Unlock foo1
+[1] Lock foo1
 Leave: test1_inner
+[0] Join
 Leave: test1
 "#);
     }
